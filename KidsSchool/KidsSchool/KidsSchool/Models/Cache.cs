97655e00da25489b60c3ca113151ac8af9e60e87
phanthanhlongit <phanthanhlongit@gmail.com>
2023-08-25 19:31:57 +0700

    [Long] commit source file cache

---
+++ KidsSchool/KidsSchool/KidsSchool/Models/Cache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+
+namespace KidsSchool.Models
+{
+    public class MemoryCacheManager
+    {
+        private static readonly MemoryCache Cache = MemoryCache.Default;
+
+        public static T Get<T>(string key)
+        {
+            return (T)Cache.Get(key);
+        }
+
+        public static void Set(string key, object value, DateTimeOffset absoluteExpiration)
+        {
+            Cache.Set(key, value, absoluteExpiration);
+        }
+
+        public static void Remove(string key)
+        {
+            Cache.Remove(key);
+        }
+    }
+
+}
